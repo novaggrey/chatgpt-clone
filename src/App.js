2091badab6eb2410bf669110b2e17eeb7ead153a
Novatus Aggrey <novataggrey47@gmail.com>
2025-03-12 16:10:12 +0300

    Deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop"><img src="" alt="" className="logo"/><span className="brand">ChatGPT Clone</span></div>
+          <button className="midBtn"><img src="" alt="" className="addBtn"/>New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src="" alt="" />What is Euphoria?</button>
+            <button className="query"><img src="" alt="" />What is Euphoria?</button>
+          </div>
+        </div>
+        <div className="lowerSide">
+
+        </div>
+      </div>
+      <div className="main">
+
+      </div>
     </div>
   );
 }
